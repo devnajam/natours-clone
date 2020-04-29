c846639988c650f0881116eb93b024534e40b783
angrynajam <najamiftikhar2020@gmail.com>
2020-04-29 11:47:04 +0500

    added sorting and removed extra logs

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,7 +11,16 @@ exports.getAllTours = async (req, res) => {
 
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    const sortBy = req.query.sort.split(',').join(' ');
+
+    if (req.query.sort) {
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
     const tours = await query;
     //testing the branches
     res.status(200).json({
