d1919d2b838858505eb7214db6fc74a9658d9d92
angrynajam <najamiftikhar2020@gmail.com>
2020-04-30 09:47:04 +0500

    pagination and limiting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,6 +28,13 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
     const tours = await query;
     //testing the branches
     res.status(200).json({
