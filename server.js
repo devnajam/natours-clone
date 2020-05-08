2a38de5742da84b59b19827b6347d975d221f2aa
angrynajam <najamiftikhar2020@gmail.com>
2020-05-08 10:50:30 +0500

    closed the error handeling module

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! SHUTTING DOWN');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -25,8 +31,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHADLED REJECTION! SHUTTING DOWN');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
