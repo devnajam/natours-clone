9473930f6c9bfe025d1791e059f97ee0dd265910
angrynajam <najamiftikhar2020@gmail.com>
2020-05-09 04:55:34 +0500

    created document middleware for password hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -32,6 +33,14 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords do not match!',
     },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
