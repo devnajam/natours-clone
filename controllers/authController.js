102b5bd5c1ff066199f81fb604cba3252e4f60fb
angrynajam <najamiftikhar2020@gmail.com>
2020-05-09 15:13:19 +0500

    created roles for users

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,6 +20,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role,
   });
 
   const token = signToken(newUser._id);
