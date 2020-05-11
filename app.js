68e4bb87367e63c19f484956c619da83fe8b937e
angrynajam <najamiftikhar2020@gmail.com>
2020-05-11 17:13:01 +0500

    using hpp

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const app = express();
 
@@ -36,6 +37,19 @@ app.use(express.json({ limit: '10kb' }));
 app.use(mongoSanitize());
 //Data sanitization against XSS
 app.use(xss());
+//prvent parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
 
 //serving static files
 app.use(express.static(`${__dirname}/public`));
