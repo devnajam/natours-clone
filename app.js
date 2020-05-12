bb30d16c032e00231bb1dc6cba9c8bb89b326e28
angrynajam <najamiftikhar2020@gmail.com>
2020-05-12 12:40:07 +0500

    creating and getting reviews

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
@@ -63,6 +64,7 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`can't find ${req.originalUrl} on this server!`, 404));
