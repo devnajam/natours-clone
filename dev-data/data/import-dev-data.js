08baf4ec2f6f8c1de3bfb6cef6cf4c6267d078a6
angrynajam <najamiftikhar2020@gmail.com>
2020-05-12 05:05:00 +0500

    embeding geojson in tours

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -19,9 +19,7 @@ mongoose
   .then(() => console.log('DB connection successfull'))
   .catch((err) => console.log(err));
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 const importData = async () => {
   try {
--- models/tourModel.js
+++ models/tourModel.js
@@ -82,6 +82,30 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    startLocation: {
+      //geojson
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
