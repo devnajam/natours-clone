6e34ccbaf96a9ecf8d564022222801fbaa4a996b
angrynajam <najamiftikhar2020@gmail.com>
2020-05-13 22:05:33 +0500

    created more factory functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -69,11 +69,8 @@ exports.createUser = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
-exports.updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
+
+//Do not update passwords wiht this
+exports.updateUser = factory.updateOne(User);
 
 exports.deleteUser = factory.deleteOne(User);
